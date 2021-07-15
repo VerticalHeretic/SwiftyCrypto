f82f15b5a87b5d2bd8da2b01791451562a5666b6
lswarss <ls.warss98@gmail.com>
2021-07-15 15:36:21 +0200

    Initial commit

--- SwiftyCrypto/ContentView.swift
+++ SwiftyCrypto/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.theme.background
+            VStack(spacing: 20) {
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                Text("Green Color")
+                    .foregroundColor(Color.theme.green)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
---
+++ SwiftyCrypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  SwiftyCrypto
+//
+//  Created by Łukasz Stachnik on 15/07/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
